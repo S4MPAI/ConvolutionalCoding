ce9fe43c81965c563796f37d1efa3bd45ccbf4ba
S4MPAI <flekshum@gmail.com>
2024-11-25 01:06:41 +0500

    add project Test

---
+++ Test/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections;
+using ConvolutionalCoding;
+
+var coder = new Encoder([13, 15]);
+var encMessage = coder.Encode([true, false, true, false]);
+Console.WriteLine($"Encoded \"1010\": {encMessage.Select(x => x ? 1 : 0).Aggregate("", (str, x) => str + x)}");
